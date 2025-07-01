2a536ffd660aa3ec77ff075a5165cf38254064ab
Ayush Mishra <117655349+ayush-mishra7@users.noreply.github.com>
2025-07-01 11:03:26 +0530

    Time: 26 ms (68.70%) | Memory: 57.5 MB (13.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        divide(nums, 0 , nums.length -1);
-        
+        divide(nums, 0 , nums.length-1);
         return nums;
 
-        
     }
     public void divide(int nums[] ,int start , int end) {
-        if (start >= end ) {
+        if (start>=end ) {
             return;
         }
-        int mid = start + (end - start)/2;
+        int mid=start+(end-start)/2;
 
         divide(nums, start, mid);
         divide(nums, mid+1, end);
@@ -18,38 +16,36 @@ class Solution {
         merge(nums, start, mid , end);
     }
     public void merge(int nums[], int start , int mid, int end) {
-        int [] merged = new int[end - start + 1];
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
+        int[] merged=new int[end-start+1];
+        int i=start;
+        int j=mid+1;
+        int k=0;
 
-        while(j <= end && i<= mid) {
-            if (nums[i] <= nums[j]) {
-                merged[k] = nums[i];
+        while(j<=end && i<=mid) {
+            if (nums[i]<=nums[j]) {
+                merged[k]=nums[i];
                 i++;
                 k++;
             }
             else {
-                merged[k] = nums[j];
+                merged[k]=nums[j];
                 j++;
                 k++;
             }
         }
         while (i<=mid) {
-            merged[k] = nums[i];
+            merged[k]=nums[i];
             i++;
             k++;
         }
         while (j<= end) {
-            merged[k] = nums[j];
+            merged[k]=nums[j];
             j++;
             k++;
         }
-        for (int b = 0; b< merged.length; b++) {
-            nums[start + b] = merged[b];
+        for (int b=0; b<merged.length; b++) {
+            nums[start+b]=merged[b];
 
         }
     }
-
-
 }
