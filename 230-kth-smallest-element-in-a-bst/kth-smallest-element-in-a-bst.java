05dc44bb5b00317fde3e58c90d65ab3242270954
Ayush Mishra <117655349+ayush-mishra7@users.noreply.github.com>
2025-07-16 09:58:36 +0530

    Time: 1 ms (29.49%) | Memory: 44.4 MB (75.23%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    //with Inorder Traversal
     private void inorder(TreeNode root, List<Integer> list){
         if(root == null) return;
         inorder(root.left, list);
